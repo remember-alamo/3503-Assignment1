84feb7257de596e12d292298293db4944ca7681e
Alan Moreno <alan@achlys.io>
2025-09-10 21:21:23 -0400

    pseudocode and structuring of the program

--- convert.c
+++ convert.c
@@ -6,3 +6,64 @@ char digit = '5';
 char binary_string[33];
 char hex_string[9];
 
+void div_convert(uint32_t n, int base, char *out){
+    char temp[65];
+    int pos = 0;
+
+    //zero case
+    if (n == 0){
+        strcpy(out, "0");
+        return;
+    }
+    // extract digits (perform division process, going through each position in the inputted digit (IMPORTANT....))
+    // ... from LEFT to RIGHT
+    while (n > 0){
+        int remainder = n % base;
+        n = n / base;
+
+        //convert digit to character
+        if (remainder < 0){ temp[pos++] = '0' + remainder; }
+        else { temp[pos++] = 'A' + (remainder - 10); }
+    }
+
+    //REVERSE the result
+    while (n > 0){
+        /*
+        1. get length of array
+        2. set up new array
+        3. pass in the results of the temp array, from bottom to top of the array
+        4. go through array again, convert any digits over 9 to their respective hex letters
+        5. pass array into *out
+        6. win
+        */
+    }
+
+
+    //TODO: figure it out dude
+
+    //don't forget about hexadecimal. so. that's probably an if statement and!!! just so you don't forget it!
+    //10: A
+    //11: B
+    //12: C
+    //13: D
+    //14: E
+    //15: F
+}
+
+void sub_convert(uint32_t n, int base, char*out)
+{
+    /*
+    1. create array buffer
+    2. begin doing math
+        z. f is the decimal number
+        a. start looping through p^n where p is the base, and n is the power position
+        b. if p^n is too big, n-1
+        c. if p^n is below f, multply it 
+        d. if p^n * 2 is still too big, multiply by 3, or 4, keep going until it's too big again
+        e. record the multiplier somewhere in the array buffer
+    3. reverse the array
+    4. pass the array buffer to *out
+    5. win!
+    */
+}
+
--- main.c
+++ main.c
@@ -5,10 +5,19 @@ int add(int a, int b){
 }
 
 int main(void){
-    int x = 3;
-    int y = 7;
-    int sum = add(x,y);
+    /*
+    the driver will mainly be here. i'll figure out this part last, because it's technically the easiset
+    i already know that you uh... probably use the file input thing and pass each line to something...
+    and use an if statement to check which function the file wants you to do...
+    from there, set up the thing that checks and tests. that might have to be another method.
+    passing things into *out will be your best friend. memory address or something can just be shared
+    like a nice ice cream with your friend :>
 
-    printf("sum of %d and %d is %d\n", x, y, sum);
+    this is probably the hardest part actually. this will be worked on after convert.c is finished.
+    */
+
+
+
+    printf("i'm old!");
     return 0;
 }
